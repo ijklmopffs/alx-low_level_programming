fe022cb13967af9edf40a2f5bf9a93b9efa83800
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-08-11 14:53:21 -0700

    Mission complete

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int a;
+
+	for(a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return(0);
+}
